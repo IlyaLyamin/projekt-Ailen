d4665ec579cc47fcf675f300006c634c3220d44f
IlyaLyamin <94840814+IlyaLyamin@users.noreply.github.com>
2022-01-23 11:33:26 +0500

    добавил звёзды на задний фон

--- center_window.py
+++ center_window.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import controls
+import random
 
 
 def first_window():
@@ -9,9 +10,15 @@ def first_window():
     pygame.display.set_caption("Ailen")
     DISP_FIRST = pygame.display.set_mode((800, 800))
     fon = pygame.image.load("дизайн/главное окно/инопланетянин.png")
-    all_sprite = pygame.sprite.Group()
-    sprite = pygame.sprite.Sprite()
+    all_sprites = pygame.sprite.Group()
+    for i in range(150):
+        star = pygame.sprite.Sprite(all_sprites)
+        star.image = pygame.image.load("дизайн/главное окно/звезда.png")
+        star.rect = star.image.get_rect()
+        star.rect.x = random.randrange(800)
+        star.rect.y = random.randrange(800)
     while True:
+        all_sprites.draw(screen)
         DISP_FIRST.blit(fon, (300, 50))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
